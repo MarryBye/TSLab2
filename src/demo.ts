c6799a9180e41986a4fef4191348b021b1647560
MarryBye <vlukianov04@gmail.com>
2025-10-06 00:32:36 +0300

    feat: change add signature to accept number[]

--- src/demo.ts
+++ src/demo.ts
@@ -1,7 +1,6 @@
 import { add, capitalize, formatNumber, Logger, type LogLevel } from './index';
 import { config } from './config';
 
-console.log('sum(typed):', add(2, 3));
 console.log('capitalize(typed):', capitalize('hello'));
 
 console.log('format(ok):', formatNumber(123.456)); // precision береться з APP_PRECISION
@@ -12,3 +11,4 @@ const logger = new Logger(config.LOG_LEVEL as LogLevel); // значення з 
 
 logger.info('Application started');
 logger.debug('Extra debug info');
+console.log('sum(2.0 ok):', add([2, 3, 4]));
